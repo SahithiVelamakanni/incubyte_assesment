f24d2d6b7fa8f0d52d25301ee610ceca3d12ea6c
sahithi velamakanni <velamakannisaisahithi@gmail.com>
2024-05-23 15:06:16 +0530

    all the other delimiters

--- assessment.rb
+++ assessment.rb
@@ -2,14 +2,30 @@
 class StringCalculator
     def add(numbers)
       raise ArgumentError, "Input must be a string" unless numbers.is_a?(String)
-      delimeter = /,|\n/
-  
-      numbers_array = numbers.split(delimeter).map(&:to_i)
+      numbers_array = extract_numbers(numbers)
       handle_negative_numbers(numbers_array)
-      numbers_array.inject(0, :+)
+      numbers_array.sum
     end
   
     private
+  
+    def extract_numbers(numbers)
+      delimiter = default_delimiter(numbers)
+      numbers.split(delimiter_regex(delimiter)).map(&:to_i)
+    end
+  
+    def default_delimiter(numbers)
+      numbers.start_with?("//") ? custom_delimiter(numbers) : /[,\n]/
+    end
+  
+    def custom_delimiter(numbers)
+      numbers.slice!(0, 2)
+      numbers.split("\n", 2)[0]
+    end
+  
+    def delimiter_regex(delimiter)
+      Regexp.new("#{delimiter}|[\n]")
+    end
   
     def handle_negative_numbers(numbers)
       negative_numbers = numbers.select { |num| num < 0 }
@@ -26,18 +42,20 @@ class StringCalculator
     end
   
     def test_add_method_should_add_the_string
-      result = @calculator.add("1,2,3")
-      assert_equal 6, result
+      assert_equal 6, @calculator.add("1,2,3")
     end
   
     def test_add_method_should_return_zero_for_empty_string
-      result = @calculator.add("")
-      assert_equal 0, result
+      assert_equal 0, @calculator.add("")
     end
+  
+    def test_add_method_should_test_with_other_delimiters
+      assert_equal 3, @calculator.add("//,\n1,\t2")
+    end
+  
     def test_add_method_should_also_pass_new_line_delimiter
-        result = @calculator.add("1,2\n8")
-        assert_equal 11, result
-      end
+      assert_equal 11, @calculator.add("1,2\n8")
+    end
   
     def test_add_method_should_raise_error_for_non_string_input
       assert_raises(ArgumentError) { @calculator.add(1) }
