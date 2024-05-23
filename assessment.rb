64a1e2bbfd9ccc095b4a2b402ef5f4060ad5901e
sahithi velamakanni <velamakannisaisahithi@gmail.com>
2024-05-23 13:24:23 +0530

    initial commit

--- assessment.rb
+++ assessment.rb
@@ -1,11 +1,19 @@
 # Service Class
 class StringCalculatorRuby
-    def initialize(param)
-      @param = param
+    def initialize
     end
     
     def add(numbers)
-        
+        if numbers.is_a?(String)
+            array = numbers.split(",")
+            result =0
+            array.each do |b|
+                result = result+b.to_i
+            end
+            return result
+        else
+            return "Value Entered is not a String"
+        end
     
     end
   end
@@ -15,11 +23,17 @@ class StringCalculatorRuby
   
   class TestYourService < Minitest::Test
 
-    def test_perform_method_should_double_input
-      service = YourService.new(5)
-      result = service.perform
+    def test_add_method_should_add_the_string
+      service = StringCalculatorRuby.new
+      result = service.add("1,2,3")
       
-      assert_equal 10, result
+      assert_equal 6, result
     end
+    def test_add_method_should_add_the_string_2
+        service = StringCalculatorRuby.new
+        result = service.add(1)
+        
+        assert_equal "Value Entered is not a String", result
+      end
   end
   
