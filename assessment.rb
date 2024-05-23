74dee9ecc9604d06ac0455df0a18a1804da6e4ca
sahithi velamakanni <velamakannisaisahithi@gmail.com>
2024-05-23 14:52:08 +0530

    Handling Negative numbers

--- assessment.rb
+++ assessment.rb
@@ -1,12 +1,18 @@
 # Service Class
 class StringCalculator
     def add(numbers)
-      if numbers.is_a?(String)
-        numbers_array = numbers.split(",").map(&:to_i)
-        numbers_array.inject(0, :+)
-      else
-        "Value Entered is not a String"
-      end
+      raise ArgumentError, "Input must be a string" unless numbers.is_a?(String)
+  
+      numbers_array = numbers.split(",").map(&:to_i)
+      handle_negative_numbers(numbers_array)
+      numbers_array.inject(0, :+)
+    end
+  
+    private
+  
+    def handle_negative_numbers(numbers)
+      negative_numbers = numbers.select { |num| num < 0 }
+      raise ArgumentError, "Negative numbers not allowed: #{negative_numbers.join(', ')}" unless negative_numbers.empty?
     end
   end
   
@@ -28,14 +34,13 @@ class StringCalculator
       assert_equal 0, result
     end
   
-    def test_add_method_should_return_error_message_for_non_string_input
-      result = @calculator.add(1)
-      assert_equal "Value Entered is not a String", result
+    def test_add_method_should_raise_error_for_non_string_input
+      assert_raises(ArgumentError) { @calculator.add(1) }
     end
   
-    def test_add_method_should_ignore_negative_numbers
-      result = @calculator.add("1,2,-3")
-      assert_equal 3, result
+    def test_add_method_should_raise_error_for_negative_numbers
+      exception = assert_raises(ArgumentError) { @calculator.add("1,2,-3") }
+      assert_equal "Negative numbers not allowed: -3", exception.message
     end
   end
   
